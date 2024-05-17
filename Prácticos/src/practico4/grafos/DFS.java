a57bd4a16295e81d75762303d2426417e5866cf6
Bernardo Michelli <bernabam@gmail.com>
2024-05-17 15:40:25 -0300

    DFS

--- Prácticos/src/practico4/grafos/DFS.java
+++ Prácticos/src/practico4/grafos/DFS.java
@@ -9,11 +9,17 @@ public class DFS<T> {
 	private static final int BLANCO = 0;
 	private static final int AMARILLO = 1;
 	private static final int NEGRO = 2;
-	private HashMap<Integer, Integer> color;
+	private int tiempo; // Esto capaz va en un arraylist dentro del hashmamp
+	private HashMap<Integer, Integer> color; // lista con color y tiempo en value?
 
 	public DFS(Grafo<T> grafo) {
 		this.grafo = grafo;
 		this.color = new HashMap<>();
+		this.tiempo = 0;
+	}
+	
+	public int getTiempo() {
+		return this.tiempo;
 	}
 
 	public void pintarDFS() {
@@ -29,23 +35,25 @@ public class DFS<T> {
 		while (adyacentesIterator.hasNext()) {
 			Integer vertice = adyacentesIterator.next();
 			if (color.get(vertice) == BLANCO) { // Retorna el valor asociado a la key
-				visitar(vertice, color);
+				DFS_visit(vertice, color);
 			}
 		}
 	}
 
-	private void visitar(Integer vertice, HashMap<Integer, Integer> color) {
+	private void DFS_visit(Integer vertice, HashMap<Integer, Integer> color) {
 		color.put(vertice, AMARILLO); // Pinto de amarillo el vertice en el q inicio
 		// Cuando entre al if un vertice de color blanco, se va a pintar de amarillo
+		tiempo++; // Este tendría q asignarse como tiempo de descubrimiento
 		
 		Iterator<Integer> adyacentesIterator = grafo.obtenerAdyacentes(vertice);
 		while (adyacentesIterator.hasNext()) {
 			Integer adyacente = adyacentesIterator.next();
 			if (color.get(adyacente) == BLANCO) { // Retorna el valor asociado a la key
-				visitar(adyacente, color); // Llamada recursiva a visitar el adyacente
+				DFS_visit(adyacente, color); // Llamada recursiva a visitar el adyacente
 			}
 		}
 		// Si se ejecutó el while completo quiere decir q todos los adyacentes fueron procesados
 		color.put(vertice, NEGRO);
+		tiempo++; // Este tendría q asignarse como tiempo de finalizacion
 	}
 }
